49bec236bab68e5ff68f002a987c0706f77f6c33
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-10 06:08:05 -0300

    Comentei o conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,15 @@
 <?php
+    // Define constantes para detalhes de conexão com o banco de dados
     define("HOST", "localhost");
     define("USER", "root");
     define("PASS", "");
     define("BASE", "biblioteca_sonhos");
 
-    $conn = new MySQLi(HOST, USER, PASS, BASE);
+    // Cria a conexão com o banco de dados
+    $conn = new mysqli(HOST, USER, PASS, BASE);
+
+    // Verifica se há erros de conexão
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
