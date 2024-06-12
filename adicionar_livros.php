d53610d80e4dc80c0735bf229f1ffa4b5c55d997
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-12 10:02:57 -0300

    atualiza o adicionar_livros.php

--- adicionar_livros.php
+++ adicionar_livros.php
@@ -7,8 +7,6 @@
 <body>
     <h1>Adicionar Livro</h1>
 
-    **Formulário para adicionar um novo livro:**
-
     <form method="post" action="adicionar_livros.php">
         <label for="titulo">Título:</label>
         <input type="text" id="titulo" name="titulo" required><br><br>
@@ -19,32 +17,41 @@
         <label for="data">Data de publicação:</label>
         <input type="date" id="data" name="data" required><br><br>
 
+        <label for="genero">Gênero:</label>
+        <input type="text" id="genero" name="genero" required><br><br>
+
+        <label for="editora">Editora:</label>
+        <input type="text" id="editora" name="editora" required><br><br>
+
         <input type="submit" name="acao" value="Salvar">
     </form>
 </body>
 </html>
 
 <?php
-include("conexao.php"); // Inclui o arquivo de conexão com o banco de dados
+include("conexao.php"); 
 
-if (isset($_POST["acao"])) { // Verifica se o formulário foi enviado
-    if ($_POST["acao"] == "salvar") { // Verifica se o valor do botão de envio é "salvar"
-        $titulo = $_POST["titulo"]; // Recupera o valor do campo "titulo"
-        $autor = $_POST["autor"]; // Recupera o valor do campo "autor"
-        $data = $_POST["data"]; // Recupera o valor do campo "data"
+if (isset($_POST["acao"])) {
 
-        if (empty($titulo) || empty($autor) || empty($data)) { // Verifica se todos os campos foram preenchidos
+    if ($_POST["acao"] == "salvar") {
+        $titulo = $_POST["titulo"];
+        $autor = $_POST["autor"];
+        $data = $_POST["data"];
+        $genero = $_POST["genero"];
+        $editora = $_POST["editora"];
+
+        if (empty($titulo) || empty($autor) || empty($data)) {
             echo "<script>alert('Preencha todos os campos obrigatórios!');</script>";
-            exit; // Interrompe a execução do código
+            exit; 
         }
 
-        $sql = "INSERT INTO livros (titulo, autor, data) VALUES ('$titulo', '$autor', '$data')"; // Cria a consulta SQL para inserir o novo livro no banco de dados
-        $resultado = $conn->query($sql); // Executa a consulta SQL
+        $sql = "INSERT INTO livros (titulo, autor, data, gênero, editora) VALUES ('$titulo', '$autor', '$data', 'genero', 'editora')";
+        $resultado = $conn->query($sql); 
 
-        if ($resultado) { // Se a consulta for bem-sucedida
+        if ($resultado) {
             echo "<script>alert('Livro adicionado com sucesso!');</script>";
-            echo "<script>location.href='ver_livros.php';</script>"; // Redireciona para a página "ver_livros.php"
-        } else { // Se a consulta falhar
+            echo "<script>location.href='ver_livros.php';</script>";
+        } else { 
             echo "<script>alert('Erro ao adicionar o livro!');</script>";
         }
     }
