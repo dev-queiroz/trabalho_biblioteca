5b39f976415fb1df7177a73516f55b11befc67dd
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-12 09:09:24 -0300

    Atualizar o conexao.php

--- conexao.php
+++ conexao.php
@@ -1,14 +1,11 @@
 <?php
-    // Define constantes para detalhes de conexão com o banco de dados
     define("HOST", "localhost");
     define("USER", "root");
     define("PASS", "");
     define("BASE", "biblioteca_sonhos");
 
-    // Cria a conexão com o banco de dados
     $conn = new mysqli(HOST, USER, PASS, BASE);
 
-    // Verifica se há erros de conexão
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
