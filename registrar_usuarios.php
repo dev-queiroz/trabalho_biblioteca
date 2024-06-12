1676cb012fc9549df0ff1ba57859b1cb7c715498
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-12 08:34:25 -0300

    Atualizar o registrar_usuarios.php

--- registrar_usuarios.php
+++ registrar_usuarios.php
@@ -3,10 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Biblioteca dos Sonhos - Registrar Usuário</title>
+  <title>Biblioteca dos Sonhos - Registrar Bibliotecário</title>
 </head>
 <body>
-  <h1>Registrar Novo Usuário</h1>
+  <h1>Registrar Novo Bibliotecário</h1>
 
   <form action="?acao=registrar" method="POST">
     <label for="nome">Nome Completo:</label>
@@ -18,42 +18,40 @@
     <label for="nascimento">Data de Nascimento:</label>
     <input type="date" id="nascimento" name="nascimento" required><br><br>
 
+    <label for="senha">Senha:</label>
+    <input type="text" id="senha" name="senha" required><br><br>
+
+   <label for="telefone">Telefone:</label>
+    <input type="int" id="telefone" name="telefone" required><br><br>
+
     <button type="submit">Registrar</button>
   </form>
 </body>
 </html>
 
 <?php
-  // Inclui o arquivo de conexão com o banco de dados
   include("conexao.php");
 
-  // Verifica se o formulário foi submetido e se a ação é "registrar"
   if (isset($_REQUEST["acao"]) && $_REQUEST["acao"] == "registrar") {
     // Pega os valores dos campos do formulário
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $nascimento = $_POST["nascimento"];
+    $senha = $_POST["senha"];
+    $telefone = $_POST["telefone"];
 
-    // Verifica se todos os campos obrigatórios foram preenchidos
-    if (empty($nome) || empty($email) || empty($nascimento)) {
-      // Se algum campo estiver vazio, exibe uma mensagem de erro
+    if (empty($nome) || empty($email) || empty($nascimento)) || empty($senha) || empty($telefone)){
       echo "<script>alert('Preencha todos os campos obrigatórios!');</script>";
-      // Interrompe a execução do código
       exit;
     }
 
-    // Cria uma consulta SQL para inserir o novo usuário no banco de dados
-    $sql = "INSERT INTO usuario (nome, email, nascimento) VALUES ('{$nome}', '{$email}', '{$nascimento}')";
-    // Executa a consulta e armazena o resultado em $resultado
+    $sql = "INSERT INTO usuario (nome, email, nascimento, senha, telefone) VALUES ('{$nome}', '{$email}', '{$nascimento}', '{senha}', '{telefone}')";
     $resultado = $conn->query($sql);
 
-    // Verifica se a inserção foi bem-sucedida
     if ($resultado) {
-      // Exibe uma mensagem de sucesso e redireciona para a página de lista de usuários
       echo "<script>alert('Usuário registrado com sucesso!');</script>";
       echo "<script>location.href='ver_usuarios.php';</script>";
     } else {
-      // Exibe uma mensagem de erro se a inserção falhar
       echo "<script>alert('Erro ao registrar o usuário!');</script>";
     }
   }
