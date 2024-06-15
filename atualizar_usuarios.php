9a84e04650fb6f69eac03b8c4a0b4aa05392150d
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-15 09:22:16 -0300

    Volta atualizar_usuarios.php

--- atualizar_usuarios.php
+++ atualizar_usuarios.php
@@ -6,7 +6,6 @@
   <title>Biblioteca dos Sonhos - Atualizar Usuário</title>
 </head>
 <body>
-
   <h1>Atualizar Usuário</h1>
 
   <?php
@@ -22,24 +21,13 @@
         $usuario = $resultado->fetch_object();
 
         echo "<form action='geral_usuarios.php?acao=atualizar&id={$id}' method='POST'>";
-
         echo "<label for='nome'>Nome Completo:</label>";
         echo "<input type='text' id='nome' name='nome' value='{$usuario->nome}' required><br><br>";
-
         echo "<label for='email'>Email:</label>";
-        echo "<input type='text' id='email' name='email' value='{$usuario->email}' required><br><br>";
-
+        echo "<input type='email' id='email' name='email' value='{$usuario->email}' required><br><br>";
         echo "<label for='nascimento'>Data de Nascimento:</label>";
         echo "<input type='date' id='nascimento' name='nascimento' value='{$usuario->nascimento}' required><br><br>";
-
-        echo "<label for='senha'>Senha:</label>";
-        echo "<input type='text' id='senha' name='senha' value='{$usuario->senha}' required><br><br>";
-
-        echo "<label for='telefone'>Telefone:</label>";
-        echo "<input type='int' id='telefone' name='telefone' value='{$usuario->telefone}' required><br><br>";
-
         echo "<button type='submit'>Atualizar</button>";
-
         echo "</form>";
       } else {
         echo "<script>alert('Usuário não encontrado!');</script>";
@@ -50,6 +38,5 @@
       echo "<script>location.href='ver_usuarios.php';</script>";
     }
   ?>
-
 </body>
 </html>
