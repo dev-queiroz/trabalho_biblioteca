efee7c246a45f510962dbe77dd53536fed4082cc
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-15 09:25:03 -0300

    Voltei registrar_usuarios.php

--- registrar_usuarios.php
+++ registrar_usuarios.php
@@ -3,26 +3,20 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Biblioteca dos Sonhos - Registrar Bibliotecário</title>
+  <title>Biblioteca dos Sonhos - Registrar Usuário</title>
 </head>
 <body>
-  <h1>Registrar Novo Bibliotecário</h1>
+  <h1>Registrar Novo Usuário</h1>
 
   <form action="?acao=registrar" method="POST">
     <label for="nome">Nome Completo:</label>
     <input type="text" id="nome" name="nome" required><br><br>
 
     <label for="email">Email:</label>
-    <input type="text" id="email" name="email" required><br><br>
+    <input type="email" id="email" name="email" required><br><br>
 
     <label for="nascimento">Data de Nascimento:</label>
     <input type="date" id="nascimento" name="nascimento" required><br><br>
-
-    <label for="senha">Senha:</label>
-    <input type="text" id="senha" name="senha" required><br><br>
-
-   <label for="telefone">Telefone:</label>
-    <input type="int" id="telefone" name="telefone" required><br><br>
 
     <button type="submit">Registrar</button>
   </form>
@@ -30,29 +24,26 @@
 </html>
 
 <?php
-  include("conexao.php");
+    include("conexao.php");
 
-  if (isset($_REQUEST["acao"]) && $_REQUEST["acao"] == "registrar") {
-    // Pega os valores dos campos do formulário
-    $nome = $_POST["nome"];
-    $email = $_POST["email"];
-    $nascimento = $_POST["nascimento"];
-    $senha = $_POST["senha"];
-    $telefone = $_POST["telefone"];
+    if (isset($_REQUEST["acao"]) && $_REQUEST["acao"] == "registrar") {
+      $nome = $_POST["nome"];
+      $email = $_POST["email"];
+      $nascimento = $_POST["nascimento"];
 
-    if (empty($nome) || empty($email) || empty($nascimento)) || empty($senha) || empty($telefone)){
-      echo "<script>alert('Preencha todos os campos obrigatórios!');</script>";
-      exit;
+      if (empty($nome) || empty($email) || empty($nascimento)) {
+        echo "<script>alert('Preencha todos os campos obrigatórios!');</script>";
+        exit;
+      }
+
+      $sql = "INSERT INTO usuario (nome, email, nascimento) VALUES ('{$nome}', '{$email}', '{$nascimento}')";
+      $resultado = $conn->query($sql);
+
+      if ($resultado) {
+        echo "<script>alert('Usuário registrado com sucesso!');</script>";
+        echo "<script>location.href='ver_usuarios.php';</script>";
+      } else {
+        echo "<script>alert('Erro ao registrar o usuário!');</script>";
+      }
     }
-
-    $sql = "INSERT INTO usuario (nome, email, nascimento, senha, telefone) VALUES ('{$nome}', '{$email}', '{$nascimento}', '{senha}', '{telefone}')";
-    $resultado = $conn->query($sql);
-
-    if ($resultado) {
-      echo "<script>alert('Usuário registrado com sucesso!');</script>";
-      echo "<script>location.href='ver_usuarios.php';</script>";
-    } else {
-      echo "<script>alert('Erro ao registrar o usuário!');</script>";
-    }
-  }
 ?>
