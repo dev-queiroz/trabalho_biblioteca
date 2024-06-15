1f0ca24502dfd7226bb9760fc2a1b6cab7b15bd4
Douglas Queiroz <152705113+dev-queiroz@users.noreply.github.com>
2024-06-15 09:26:06 -0300

    Voltei ver_usuarios.php

--- ver_usuarios.php
+++ ver_usuarios.php
@@ -7,6 +7,7 @@
 </head>
 <body>
   <h1>Lista de Usuários</h1>
+
   <table>
     <thead>
       <tr>
@@ -14,18 +15,16 @@
         <th>Nome</th>
         <th>Email</th>
         <th>Nascimento</th>
-        <th>Senha</th>
-        <th>Telefone</th>
         <th>Ações</th>
       </tr>
     </thead>
     <tbody>
       <?php
         include("conexao.php");
-      
+
         $sql = "SELECT * FROM usuario";
         $resultado = $conn->query($sql);
-      
+
         if ($resultado->num_rows > 0) {
           while ($usuario = $resultado->fetch_object()) {
             echo "<tr>";
@@ -33,19 +32,17 @@
             echo "<td>{$usuario->nome}</td>";
             echo "<td>{$usuario->email}</td>";
             echo "<td>{$usuario->nascimento}</td>";
-            echo "<td>{$usuario->senha}</td>";
-            echo "<td>{$usuario->telefone}</td>";
-            echo "<td>";usuário
+            echo "<td>";
             echo "<a href='atualizar_usuarios.php?id={$usuario->id}'>Editar</a>";
             echo " | ";
             echo "<a href='geral_usuarios.php?acao=excluir&id={$usuario->id}' onclick='return confirm(\"Deseja realmente excluir este usuário?\");'>Excluir</a>";
             echo "</td>";
             echo "</tr>";
           }
-         } else {
-            echo "<tr><td colspan='5'>Nenhum usuário cadastrado.</td></tr>";
-         }
-       ?>
+        } else {
+          echo "<tr><td colspan='5'>Nenhum usuário cadastrado.</td></tr>";
+        }
+      ?>
     </tbody>
   </table>
 </body>
